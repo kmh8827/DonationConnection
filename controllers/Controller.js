ac55ca12143a26881d8010678331c720a71fe6dd
kevin <kevinhellman94@gmail.com>
2021-04-26 20:52:53 -0400

    Updated Controler for User Creation, Deletion, and Password Changes

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -14,22 +14,41 @@ module.exports = {
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-    create: (req, res) => {
+    addDonation: (req, res) => {
         db.Donations
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-    remove: (req, res) => {
+    removeDonation: (req, res) => {
         db.Donations
             .findById({ _id: req.params.id })
             .then(dbModel => dbModel.remove())
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-    findById: (req, res) => {
+    findDonation: (req, res) => {
         db.Donations
             .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    addUser: (req, res) => {
+        db.Users
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    deleteUser: (req, res) => {
+        db.Users
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    changePasswordUser: (req, res) => {
+        db.Users
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     }
