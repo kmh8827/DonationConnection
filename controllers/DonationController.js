cb9c519d9ffb3bb056b45f88647727d0c50cc562
kevin <kevinhellman94@gmail.com>
2021-04-26 21:13:27 -0400

    Updated Donation Controller to sort by Allergies

--- controllers/DonationController.js
+++ controllers/DonationController.js
@@ -32,5 +32,12 @@ module.exports = {
             .findById(req.params.id)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
+    },
+    sortDonation: (req, res) => {
+        db.Donations
+            .find({ })
+            .where(req.body).in(allergies)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     }
 };
