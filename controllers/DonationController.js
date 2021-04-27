108a181f18e1be7e55615ab41b62a599bdc5d15b
kevin <kevinhellman94@gmail.com>
2021-04-26 20:59:08 -0400

    Seperated User and Donation Controllers into two files"

--- controllers/DonationController.js
+++ controllers/DonationController.js
@@ -32,24 +32,5 @@ module.exports = {
             .findById(req.params.id)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
-    },
-    addUser: (req, res) => {
-        db.Users
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    deleteUser: (req, res) => {
-        db.Users
-            .findById({ _id: req.params.id })
-            .then(dbModel => dbModel.remove())
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    changePasswordUser: (req, res) => {
-        db.Users
-            .findOneAndUpdate({ _id: req.params.id }, req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
     }
 };
