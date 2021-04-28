1aaf6855159bf36b2e98096fcdd315eba5be50f8
kevin <kevinhellman94@gmail.com>
2021-04-28 19:40:01 -0400

    Updating Server File with new dependencies, checking if NOT production, updating middlewares with session

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
+// Loading Environmnetal Variables
+if (process.env.NODE_ENV !== 'production') {
+    console.log('loading dev environments');
+    require('dotenv').config();
+}
+require('dotenv').config();
 
-const mongoose = require('mongoose');
+const express = require('express');
+const session = require('express-session');
+const passport = require('./passport');
+const MongoStore = requrie('connection-mongo')(session);
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session({
+    secret: process.env.APP_SECRET || 'this is the default pasphrase',
+    store: new MongoStore({ mongooseConnection: dbConnection }),
+    resave: false,
+    saveUninitialized: false
+}));
 
 if (process.env.NODE_ENV === 'production') app.use(express.static('client/build'));
 
