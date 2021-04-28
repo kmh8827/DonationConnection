9488f33305ec72466f712eedf5e6dc8de9bd10e4
Joe Basquez <jybasquez@yahoo.com>
2021-04-28 19:39:02 -0400

    return form skeleton

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -1,19 +1,51 @@
 import React from "react";
 class Form extends React.Component{
     state={
-        address: "",
-        allegies: "",
         companyName:"",
-        expiration: "",
+        address: "",
         parishable: "",
-        specialInstructions: "",
-        quantity: ""
+        expiration: "",
+        quantity: "",
+        allegies: "",
+        specialInstructions: ""
     };
 
     render(){
         return (
             <div>
-                <p>testing out this app</p>
+                <p>Donation Form</p>
+                <form>
+                    <h3>Company Name</h3>
+                    <input 
+                    value={this.state.companyName}
+                    type="text"
+                    placeholder="Company Name"
+                    />
+                    <h3>Address</h3>
+                    <input 
+                    value={this.state.address}
+                    type="text"
+                    placeholder="Address"
+                    />
+                    <h3>Perishable</h3>
+                    <input 
+                    value={this.state.perishable}
+                    type="boolean"
+                    placeholder="yes or no"
+                    />
+                    <h3>Expiration</h3>
+                    <input 
+                    value={this.state.expiration}
+                    type="date"
+                    />
+                    <h3>How many?</h3>
+                    <input 
+                    value={this.state.quantity}
+                    type="number"
+                    placeholder="#"
+                    />
+                    
+                </form>
             </div>
         )
     }
