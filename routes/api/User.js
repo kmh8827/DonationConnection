83cb896be31bbec7c009e7a1b4f2c1fe014ab3fb
kevin <kevinhellman94@gmail.com>
2021-04-28 20:15:45 -0400

    Moved Authentication Folder, Removed User.js and updated Routes Index.js

--- routes/api/User.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router;
-const userController = require('../../controllers/UserController');
-
-router.route('/login')
-    .post(userController.addUser)
-    .post(userController.changePasswordUser)
-    .delete(userController.deleteUser)
-
-module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const donationRoutes = require('./Donation');
-const userRoutes = require('./User');
 
 router.use('/donations', donationRoutes);
-router.use('/user', userRoutes);
 
 module.exports = router;
