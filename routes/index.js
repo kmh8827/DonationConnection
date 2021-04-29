e2408d248109662fc53f6259eb780a016b6cbdb8
kevin <kevinhellman94@gmail.com>
2021-04-28 20:14:13 -0400

    Added Authentication folder and Index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const path = require('path');
 const router = require('express').Router();
+const authRoutes = require('./auth');
 const apiRoutes = require('./api');
 
+// Authentication Routes
+router.use('/auth', authRoutes);
+
+// API Routes
 router.use('/api', apiRoutes);
 
+// If no routes are hit, send to React Application
 router.use((req, res) => {
     res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
