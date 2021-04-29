6656b14aa8efe7f66ca8078e3b456f6af1639c5d
alexhstrickland <alexhstrickland@gmail.com>
2021-04-28 20:53:36 -0400

    added bg image and starter content to the home page

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./home.css";
 
 function Home() {
-    return();
+    return(
+        <div>
+            <div className="bg-image"></div>
+
+            <div className="bg-text">
+                <h1>Donation Connection</h1>
+                <p>Where hunger meets a helping hand.</p>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
