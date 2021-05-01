c4b6fbf64bb35febfb11d569405aaa23d967b822
kevin <kevinhellman94@gmail.com>
2021-05-01 14:01:27 -0400

    Preparing to pull

--- client/src/components/reserveCard.js
+++ client/src/components/reserveCard.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {
+    Card, CardText, CardBody,
+    CardTitle, Button
+  } from 'reactstrap';
 
 const reserveCard = (props) => {
+  return (
     <div>
-        <button color="primary">Reserve</button>
+        <Card className="reserveCard">
+        <CardBody>
+          <CardTitle tag="h5">{props.companyName}</CardTitle>
+          <CardText>
+              <ul>
+              <li>Perishable: {props.perishable}</li>
+              <li>Expiration Date: {props.expDate}</li>
+              <li>Availablity: {props.availability}</li>
+              <li>Address: {props.address}</li>
+              {/* Array of Allergies */}
+              </ul>
+          </CardText>
+          <Button color="primary">Reserve</Button>
+        </CardBody>
+      </Card>
     </div>
+  )
 }
 
 export default reserveCard;
--- client/src/pages/pickup.js
+++ client/src/pages/pickup.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import reserveCard from '../components/reserveCard';
 
 const Pickup = () => {
+    const [data, setData] = useState({ donations: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+        const result = await axios.get('/');
+        setData(result.data);
+        };
+        fetchData();
+    }, []);
+
+    return (
     <div>
         <reserveCard />
     </div>
+    )
 }
 
 export default Pickup;
