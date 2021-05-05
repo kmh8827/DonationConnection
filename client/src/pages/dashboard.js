8e5373a5108c3bb77811bbdb905add9c62857bcd
mdu830 <mdu830@yahoo.com>
2021-05-05 18:56:57 -0400

    fixed button styling errors between home and dashboard

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -14,13 +14,14 @@ const Dashboard = (props) => {
             <div className="container">
                 <div className="row ml-5 mt-5">
                     <div className="pickup col">
-                        <div className="btn btn-light m-4 mt-5">
+                        <div className="btn btn-dark pickupBtn m-4 mt-5">
                             <h5 className="p-3 text-center">Pickup</h5>
+                            <span className="tooltip">Find local donations available for pickup</span>
                             <img id="pickupImg" className="m-3 mx-auto image-fluid" alt="Pickup" src={pickup}/>
                         </div>
                     </div>
                     <div className="donate col">
-                        <div className="btn btn-light m-4 mt-5">
+                        <div className="btn btn-dark m-4 mt-5">
                             <h5 className="p-3 text-center">Donate</h5>
                             <img id="donateImg" className="mt-n3 m-3 mx-auto image-fluid" alt="Pickup" src={donate}/>
 
