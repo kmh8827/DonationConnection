b78ba288ac01ed08d952c2f9ceb19464a9e37735
kevin <kevinhellman94@gmail.com>
2021-05-05 11:26:28 -0400

    Added User Authentication API Routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,17 +7,11 @@ export default {
     reserveDonations: (id) => {
         return axios.post('/api/donations/receive/' + id)
     },
-    // myDonations: () => {
-    //     return axios.get('api/donations/give')
-    // },
     newDonation: (donation) => {
         return axios({ 
             method: 'POST',
             url: 'api/donations/give',
             data: donation
         })
-    },
-    removeDonation: (id) => {
-        return axios.delete('api/donations/give' + id)
     }
 };
---
+++ client/src/utils/AUTH.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default {
+    // Get user informaiton
+    getUser: () => {
+        return axios.get('/auth/user');
+    },
+    // Log the user out
+    logout: () => {
+        return axios.post('/auth/logout');
+    },
+    // Log the user in
+    login: (username, password) => {
+        return axios.post('/auth/login', { username, password });
+    },
+    // Allows a new user to register
+    signup: (userData) => {
+        return axios.post('/auth/signup', userData);
+    }
+};
