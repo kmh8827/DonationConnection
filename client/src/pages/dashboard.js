b77d056fd8446afa7468188c724da2cb88d2a3b8
mdu830 <mdu830@yahoo.com>
2021-05-05 17:36:41 -0400

    added better button icons and added more styl to pickup and donate buttons

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -12,15 +12,15 @@ const Dashboard = (props) => {
         <div className="bg-image">
             <Header />
             <div className="container">
-                <div className="row">
+                <div className="row ml-5">
                     <div className="pickup col">
-                        <div className="card m-4 mt-5">
+                        <div className="btn btn-light m-4 mt-5">
                             <h5 className="p-3 text-center">Pickup</h5>
                             <img id="pickupImg" className="m-3 mx-auto image-fluid" alt="Pickup" src={pickup}/>
                         </div>
                     </div>
                     <div className="donate col">
-                        <div className="card m-4 mt-5">
+                        <div className="btn btn-light m-4 mt-5">
                             <h5 className="p-3 text-center">Donate</h5>
                             <img id="donateImg" className="mt-n3 m-3 mx-auto image-fluid" alt="Pickup" src={donate}/>
 
