4dc5a5ab187875c09381de6a7367b60ca2076c9a
mdu830 <mdu830@yahoo.com>
2021-05-05 16:57:23 -0400

    added dashboard scss and started styling

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import "../assets/scss/dashboard.scss";
+import '../assets/scss/dashboard.scss';
+import pickup from '../assets/images/icons/pickup.png';
+import donate from '../assets/images/icons/donate.png';
+import Header from '../components/header'
 
 
 
@@ -7,13 +10,21 @@ const Dashboard = (props) => {
     return (
         <>
         <div className="bg-image">
+            <Header />
             <div className="container">
                 <div className="row">
                     <div className="pickup col">
-                        
+                        <div className="card m-4 mt-5">
+                            <h5 className="p-3 text-center">Pickup</h5>
+                            <img id="pickupImg" className="m-3 mx-auto image-fluid" alt="Pickup" src={pickup}/>
+                        </div>
                     </div>
                     <div className="donate col">
+                        <div className="card m-4 mt-5">
+                            <h5 className="p-3 text-center">Donate</h5>
+                            <img id="donateImg" className="mt-n3 m-3 mx-auto image-fluid" alt="Pickup" src={donate}/>
 
+                        </div>
                     </div>
                 </div>
             </div>
