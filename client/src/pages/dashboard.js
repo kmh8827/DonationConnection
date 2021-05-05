bf501feeb35f59fc504ca0474cb2b85a68a670de
mdu830 <mdu830@yahoo.com>
2021-05-05 19:40:06 -0400

    added tooltips for dashboard buttons

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTooltip from "react-tooltip";
 import '../assets/scss/dashboard.scss';
 import pickup from '../assets/images/icons/pickup.png';
 import donate from '../assets/images/icons/donate.png';
@@ -14,18 +15,22 @@ const Dashboard = (props) => {
             <div className="container">
                 <div className="row ml-5 mt-5">
                     <div className="pickup col">
-                        <div className="btn btn-dark bigBtn pickupBtn m-4 mt-5">
+                        <div data-tip data-for="pickupInfo" className="btn btn-dark bigBtn pickupBtn m-4 mt-5">
                             <h5 className="p-3 text-center">Pickup</h5>
-                            <span className="tooltip">Find local donations available for pickup</span>
                             <img id="pickupImg" className="m-3 mx-auto image-fluid" alt="Pickup" src={pickup}/>
                         </div>
+                        <ReactTooltip id="pickupInfo" place="top" effect="solid">
+                            Find available donations for pickup near you
+                        </ReactTooltip>
                     </div>
                     <div className="donate col">
-                        <div className="btn btn-dark bigBtn m-4 mt-5">
+                        <div data-tip data-for="donationInfo" className="btn btn-dark bigBtn m-4 mt-5">
                             <h5 className="p-3 text-center">Donate</h5>
                             <img id="donateImg" className="mt-n3 m-3 mx-auto image-fluid" alt="Pickup" src={donate}/>
-
                         </div>
+                        <ReactTooltip id="donationInfo" place="top" effect="solid">
+                            Post available donation(s) in your area
+                        </ReactTooltip>
                     </div>
                 </div>
             </div>
