23ee6ef873308262fbceba0d944b6912be996c00
kevin <kevinhellman94@gmail.com>
2021-05-05 20:56:55 -0400

    Preparing to pull

--- server.js
+++ server.js
@@ -29,12 +29,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 if (process.env.NODE_ENV === 'production') {
-    const path= require('path');
-    app.use('static', express.static(path.join(__dirname, './client/build/static')));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/'));
-    });
-}
+    app.use(express.static('client/build'));
+  }  
 
 // Adding API Routes
 app.use(routes);
