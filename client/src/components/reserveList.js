6f524c4e9293b7d68601b01720f3e1b2d4abb4d5
kevin <kevinhellman94@gmail.com>
2021-05-05 20:13:01 -0400

    Creating App On Heroku

--- client/src/components/reserveList.js
+++ client/src/components/reserveList.js
@@ -7,11 +7,7 @@ const reserveList = (props) => {
                 <li>Expiration Date: {props.expDate}</li>
                 <li>Availablity: {props.availability}</li>
                 <li>Address: {props.address}</li>
-                  <ul>
-                  {props.allergies.options.map(theseAllergies => 
-                  <li key={theseAllergies}>{theseAllergies}</li>  
-                  )}
-                  </ul>
+                <li>Allergies: {props.allergies}</li>
                 <li>Other Allergy Information: {props.allergies.otherInfo ? props.allergies.otherInfo : 'None'}</li>
               </ul> 
         </div>
