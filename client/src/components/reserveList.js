3756f59f487fc1a77e70d155da96a1c818635074
mdu830 <mdu830@yahoo.com>
2021-05-08 12:02:05 -0400

    set dashboard background to the bg-image-home class

--- client/src/components/reserveList.js
+++ client/src/components/reserveList.js
@@ -1,15 +1,14 @@
 const reserveList = (props) => {
     return(
-        <div>
-             <ul>
-                <li>Comapny Name: {props.companyName}</li>
-                <li>Perishable: {props.perishable}</li>
-                <li>Expiration Date: {props.expDate}</li>
-                <li>Availablity: {props.availability}</li>
-                <li>Address: {props.address}</li>
-                <li>Allergies: {props.allergies}</li>
-              </ul> 
-        </div>
+        <ul>
+            <li>Comapny Name: {props.companyName}</li>
+            <li>Perishable: {props.perishable}</li>
+            <li>Expiration Date: {props.expDate}</li>
+            <li>Availablity: {props.availability}</li>
+            <li>Address: {props.address}</li>
+            <li>Allergies: {props.allergies}</li>
+        </ul> 
+        
     )
 }
 
