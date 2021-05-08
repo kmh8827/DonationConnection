e896d19987f30b7bc1f9c964ee5a7fdb55d89cab
mdu830 <mdu830@yahoo.com>
2021-05-08 13:43:01 -0400

    changed form route and added register route in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Form from "./pages/donationForm";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 import Pickup from "./pages/pickup";
-import Donation from "./pages/donation";
+import Register from "./pages/register";
 import Footer from "./components/footer";
 function App() {
   return (
@@ -12,12 +12,12 @@ function App() {
         <Router>
           <Switch>
             <Route exact path={["/"]} component={Dashboard} />
-            <Route exact path={["/form"]} component={Form} />
+            <Route exact path={["/donation"]} component={Form} />
             <Route exact path={["/login"]} component={Login} />
             <Route exact path={["/dashboard"]} component={Dashboard} />
             <Route exact path={["/home"]} component={Home} />
             <Route exact path={["/pickup"]} component={Pickup}/>
-            <Route exact path={["/donation"]} component={Donation} />
+            <Route exact path={["/register"]} component={Register} />
           </Switch>
           <Footer />
         </Router>
