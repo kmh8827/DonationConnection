b915e42d3b693cd6654bcfb7a61bd0aa101fb192
mdu830 <mdu830@yahoo.com>
2021-05-08 11:55:15 -0400

    added pickup.scss and started styling pickup page and components

--- client/src/pages/pickup.js
+++ client/src/pages/pickup.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import API from '../utils/API';
 import ReserveCard from '../components/reserveCard';
+import '../assets/scss/pickup.scss';
 
 const Pickup = () => {
     const [donations, setDonations] = useState([]);
@@ -20,20 +21,22 @@ const Pickup = () => {
     };
 
     return (
-    <div>
-        {donations && donations.map(thisDonation => 
-            <ReserveCard 
-                key={thisDonation._id}
-                product={thisDonation.product}
-                companyName={thisDonation.companyName}
-                perishable={thisDonation.perishable}
-                expDate={thisDonation.expDate}
-                availability={thisDonation.availability}
-                address={thisDonation.address}
-                allergies={thisDonation.allergies}
-                id={thisDonation._id}
-            />
-        )}
+    <div className="bg-image-pickup">
+        <div className="container">
+            {donations && donations.map(thisDonation => 
+                <ReserveCard 
+                    key={thisDonation._id}
+                    product={thisDonation.product}
+                    companyName={thisDonation.companyName}
+                    perishable={thisDonation.perishable}
+                    expDate={thisDonation.expDate}
+                    availability={thisDonation.availability}
+                    address={thisDonation.address}
+                    allergies={thisDonation.allergies}
+                    id={thisDonation._id}
+                />
+            )}
+        </div>
     </div>
     );
 }
