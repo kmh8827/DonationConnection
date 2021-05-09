26da0df41ae9a1c7c7e02d54042e701bca18355d
mdu830 <mdu830@yahoo.com>
2021-05-09 12:26:16 -0400

    added more styling to navbar

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,21 +4,21 @@ import '../assets/scss/nav.scss'
 function NavBar() {
   return (
 
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <div className="navbar-brand">Donation Connection</div>
+    <nav className="navbar navTitleBar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div className="navbar-brand appTitle">Donation Connection</div>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
+      <div className="collapse navbar-collapse text-right" id="navbarNav">
+        <ul className="navbar-nav ml-auto pl-0">
           <li className="nav-item active">
-            <a className="nav-link" href="/home">Home</a>
+            <a className="nav-link navBtn" href="/home">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/login">Login</a>
+            <a className="nav-link navBtn" href="/login">login</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/form">Donate Now!</a>
+            <a className="nav-link navBtn" href="/account">Account</a>
           </li>
         </ul>
       </div>
