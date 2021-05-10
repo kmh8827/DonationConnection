ac46ef455abddc5537486b79848e58290e5d446d
Joe Basquez <jybasquez@yahoo.com>
2021-05-10 19:43:40 -0400

    some styling done

--- client/src/pages/errorPage.js
+++ client/src/pages/errorPage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../assets/scss/errorPage.scss"
+
 class Error extends React.Component {
     render(){
     return(
         <div>
-                <h3 className="errorh3">You shouldnt be here!</h3>
+                <div classname="errorImage">
+                <h3 className="errorh3">Whoopsss! You shouldnt be here!</h3>
                 <div className="margin">
-                <button type="button" className="btn btn-light" onClick={event =>  window.location.href='/'}>Click Here To Return</button>
+                <button type="button" className="btn btn-light return" onClick={event =>  window.location.href='/'}>Click Here To Return</button>
+                </div>
                 </div>
         </div>
     )}
