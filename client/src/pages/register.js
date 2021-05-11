d0830513d1f4d9510f869acc4089e4f18c752b6b
elenamagay <elenamagay1985@gmail.com>
2021-05-10 20:46:54 -0400

    added password line to Register page

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -52,37 +52,55 @@ const Register = () => {
     return (
         <div>
             <div className="container">
-            <form className="row g-3 needs-validation" novalidate>
-                <div className="col-md-4">
-                    <label for="validationCustom01" className="form-label">First name</label>
-                    <input type="text" className="form-control" id="validationCustom01" value="Mark" required />
-                    <div className="valid-feedback">
-                    Looks good!
+                <h2>New User Registration Form</h2><br/>
+                <form className="row g-3 needs-validation" novalidate>
+                    <div className="col-md-4">
+                        <label for="validationCustom01" className="form-label">First name</label>
+                        <input type="text" className="form-control" id="validationCustom01" onChange={handleChange} value={userObject.firstName} required/>
+                        <div className="valid-feedback">
+                        Looks good!
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-4">
-                    <label for="validationCustom02" className="form-label">Last name</label>
-                    <input type="text" className="form-control" id="validationCustom02" value="Otto" required />
-                    <div className="valid-feedback">
-                    Looks good!
+                    <div className="col-md-4">
+                        <label for="validationCustom02" className="form-label">Last name</label>
+                        <input type="text" className="form-control" id="validationCustom02" onChange={handleChange} value={userObject.lastName} required />
+                        <div className="valid-feedback">
+                        Looks good!
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-4">
-                    <label for="validationCustomUsername" className="form-label">Username</label>
-                    <div className="input-group has-validation">
-                    <span className="input-group-text" id="inputGroupPrepend">@</span>
-                    <input type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
-                    <div className="invalid-feedback">
-                        Please choose a username.
+                    <div className="col-md-4">
+                        <label for="validationCustomUsername" className="form-label">Username</label>
+                        <div className="input-group has-validation">
+                        <span className="input-group-text" id="inputGroupPrepend">@</span>
+                        <input type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" onChange={handleChange} value={userObject.username} required />
+                        <div className="invalid-feedback">
+                            Please choose a username.
+                        </div>
+                        </div>
                     </div>
+                    <div className="col-md-4">
+                        <label for="validationCustomPassword" className="form-label">Password</label>
+                        <div className="input-group has-validation">
+                        <input type="password" className="form-control" id="validationCustomPassword" aria-describedby="inputGroupPrepend" onChange={handleChange} value={userObject.password} required />
+                        <div className="invalid-feedback">
+                            Please enter a password.
+                        </div>
+                        </div>
                     </div>
+                    <div className="col-md-4">
+                        <label for="validationCustomPassword" className="form-label">Confirm password</label>
+                        <div className="input-group has-validation">
+                        <input type="password" className="form-control" id="validationCustomConfirmPassword" aria-describedby="inputGroupPrepend" onChange={handleChange} value={userObject.confirmPassword} required />
+                        <div className="invalid-feedback">
+                            Please confirm your password.
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <button className="btn btn-primary" onClick={handleSubmit}>Register</button>
+                    </div>
+                    </form>
                 </div>
-                
-                <div className="col-12">
-                    <button className="btn btn-primary" type="submit">Submit form</button>
-                </div>
-                </form>
-            </div>
         </div>
     );
 }
