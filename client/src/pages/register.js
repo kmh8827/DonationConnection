7e2a0a4955cd2c8402797d9c00d8dbace2222793
kevin <kevinhellman94@gmail.com>
2021-05-12 09:37:35 -0400

    Registering new Users Functional

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Redirect, Link } from "react-router-dom";
-import axios from "axios";
+import AUTH from "../utils/AUTH";
 
 const Register = () => {
     const [userObject, setUserObject] = useState({
@@ -9,11 +9,12 @@ const Register = () => {
         username: "",
         password: "",
         confirmPassword: "",
+        redirectTo: null
     });
 
     const [redirectTo, setRedirectTo] = useState(null);
-    const [registered, setRegistered] = useState(false);
-    const [error, setError] = useState(null);
+    // const [registered, setRegistered] = useState(false);
+    // const [error, setError] = useState(null);
 
     const handleChange = (e) => {
         setUserObject({
@@ -25,22 +26,17 @@ const Register = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const submit = (userData) => {
-            return axios.post("/register", userData);
-        };
-
-        submit({
+        AUTH.signup({
             firstName: userObject.firstName,
             lastName: userObject.lastName,
             username: userObject.username,
             password: userObject.password,
         })
         .then(response => {
-            if (response.data.error) {
-                setError(response.data.error)
+            if (!response.data.errmsg) {
+                setRedirectTo('/')
             } else {
-                setError(false);
-                setRegistered(true);
+                console.log('duplicate');
             }
         });
 
@@ -56,14 +52,26 @@ const Register = () => {
                 <form className="row g-3 needs-validation" novalidate>
                     <div className="col-md-4">
                         <label for="validationCustom01" className="form-label">First name</label>
-                        <input type="text" className="form-control" id="validationCustom01" onChange={handleChange} value={userObject.firstName} required/>
+                        <input type="text"
+                        name="firstName"
+                        className="form-control" 
+                        id="validationCustom01" 
+                        onChange={handleChange} 
+                        value={userObject.firstName} 
+                        required/>
                         <div className="valid-feedback">
                         Looks good!
                         </div>
                     </div>
                     <div className="col-md-4">
                         <label for="validationCustom02" className="form-label">Last name</label>
-                        <input type="text" className="form-control" id="validationCustom02" onChange={handleChange} value={userObject.lastName} required />
+                        <input type="text" 
+                        name="lastName"
+                        className="form-control" 
+                        id="validationCustom02" 
+                        onChange={handleChange} 
+                        value={userObject.lastName} 
+                        required />
                         <div className="valid-feedback">
                         Looks good!
                         </div>
@@ -72,7 +80,14 @@ const Register = () => {
                         <label for="validationCustomUsername" className="form-label">Username</label>
                         <div className="input-group has-validation">
                         <span className="input-group-text" id="inputGroupPrepend">@</span>
-                        <input type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" onChange={handleChange} value={userObject.username} required />
+                        <input type="text"
+                        name="username"
+                        className="form-control" 
+                        id="validationCustomUsername" 
+                        aria-describedby="inputGroupPrepend" 
+                        onChange={handleChange} 
+                        value={userObject.username} 
+                        required />
                         <div className="invalid-feedback">
                             Please choose a username.
                         </div>
@@ -81,7 +96,14 @@ const Register = () => {
                     <div className="col-md-4">
                         <label for="validationCustomPassword" className="form-label">Password</label>
                         <div className="input-group has-validation">
-                        <input type="password" className="form-control" id="validationCustomPassword" aria-describedby="inputGroupPrepend" onChange={handleChange} value={userObject.password} required />
+                        <input type="password" 
+                        name="password"
+                        className="form-control" 
+                        id="validationCustomPassword" 
+                        aria-describedby="inputGroupPrepend" 
+                        onChange={handleChange} 
+                        value={userObject.password} 
+                        required />
                         <div className="invalid-feedback">
                             Please enter a password.
                         </div>
@@ -90,7 +112,14 @@ const Register = () => {
                     <div className="col-md-4">
                         <label for="validationCustomPassword" className="form-label">Confirm password</label>
                         <div className="input-group has-validation">
-                        <input type="password" className="form-control" id="validationCustomConfirmPassword" aria-describedby="inputGroupPrepend" onChange={handleChange} value={userObject.confirmPassword} required />
+                        <input type="password" 
+                        name="confirmPassword"
+                        className="form-control" 
+                        id="validationCustomConfirmPassword" 
+                        aria-describedby="inputGroupPrepend" 
+                        onChange={handleChange} 
+                        value={userObject.confirmPassword} 
+                        required />
                         <div className="invalid-feedback">
                             Please confirm your password.
                         </div>
