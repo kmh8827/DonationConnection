570c44c0b24b97aead05996eb91f79f110085746
kevin <kevinhellman94@gmail.com>
2021-05-15 13:38:48 -0400

    Perparing to pull

--- client/src/pages/accountInfo.js
+++ client/src/pages/accountInfo.js
@@ -6,6 +6,7 @@ import "../assets/scss/accountInfo.scss";
 const AccountInfo = () => {
     const { user } = useContext(CurrentUserContext);
     const [donations, setDonations] = useState(null);
+    let returned = false;
 
     useEffect(() => {
         const donationCall = {
@@ -15,18 +16,18 @@ const AccountInfo = () => {
         API.myDonations(donationCall).then(response => {
             console.log(response.data);
             setDonations(response.data);
+            returned = true;
+            console.log(returned);
         })
-    }, [user]);
+    }, [user, returned]);
 
     return (
         <div>
-            <p>Hi</p>
             <div>
                 <div className="companyAbout">
                     <h2 className="companyAbout accInfo">Account Info:</h2>
                     <div>
-                        <label className="form-label">Username:</label>
-                        <p>{user.username}</p>
+                        <label className="form-label">Username: {user.username}</label>
                     </div>
                     <div>
                         <label className="form-label">Email:</label>
@@ -39,8 +40,15 @@ const AccountInfo = () => {
                 </div>
                 <React.Fragment>
                     <label className="form-label status onHold">Donations on hold:</label>
+
                     <label className="form-label status available">Available Donations:</label>
+                        <ul>
+                            { !returned ? "" : donations.map(thisDonation =>
+                                <li>{thisDonation}</li>
+                            )}
+                        </ul>
                     <label className="form-label status pickedup">Picked-up Donations:</label>
+
                 </React.Fragment>
             </div>
         </div>
