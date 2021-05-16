264f1b4a07e6dc0a62cd3c6a7757c73e16333a8c
mdu830 <mdu830@yahoo.com>
2021-05-16 12:27:56 -0400

    added old seeds back to the seeders.js

--- client/src/pages/pickup.js
+++ client/src/pages/pickup.js
@@ -43,7 +43,7 @@ const Pickup = () => {
         })
                 
         setDonations(newOpened)    
-      }
+    }
 
     const reserved = donations.availability;
 
@@ -61,10 +61,10 @@ const Pickup = () => {
             <Header />
             <div className="pickupContainer">
                 <GridList style={gridTileStyle}>
-                    {/* {console.log(donations, 'donations')} */}
+
                     {/* need next and previous buttons for different slice(x, x) */}
                     
-                    {donations && donations.slice(6, 12).map(thisDonation => {
+                    {donations && donations.slice(0, 6).map(thisDonation => {
                         // console.log(thisDonation._id)
                      return   (
                          
