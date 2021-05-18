96548c8675fe04744ae8d3e595addc0771f5d8f9
kevin <kevinhellman94@gmail.com>
2021-05-18 12:06:01 -0400

    Updated Pathing to use history"

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,35 +1,38 @@
 import React, { useContext } from 'react';
+import { useHistory } from "react-router-dom";
 import '../assets/scss/nav.scss'
 import BrandIcon from '../assets/images/icons/BrandIcon.png'
 import AUTH from "../utils/AUTH";
 import { CurrentUserContext } from "../context/currentUser";
 import { Dropdown } from "react-bootstrap";
 
-function Header() {
+const Header = () => {
 
-    const { user, handleSetUser } = useContext(CurrentUserContext);
+  const { user, handleSetUser } = useContext(CurrentUserContext);
+  const history = useHistory();
 
   const logout = (e) => {
     e.preventDefault();
     AUTH.logout().then(() => {
       handleSetUser({});
-    }); 
+      history.push('/');
+    });
   };
 
   // Dropdown needs access to the DOM node in order to position the Menu
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-  <a id="dropdownToggle" href="" ref={ref} onClick={(e) => {
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a id="dropdownToggle" href="" ref={ref} onClick={(e) => {
       e.preventDefault();
-      onClick(e); 
-  }}>
-    {children}
+      onClick(e);
+    }}>
+      {children}
     &#x25BE;
-  </a>
-));
+    </a>
+  ));
 
   return (
     <nav className="nav navbar navTitleBar navbar-expand-md fixed-top">
-      <img id="brandIcon" className=" image-fluid" alt="Pickup" src={BrandIcon}/>
+      <img id="brandIcon" className=" image-fluid" alt="Pickup" src={BrandIcon} />
       <div className="navbar-brand appTitle ml-4">Donation Connection</div>
       <div className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -37,28 +40,22 @@ const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
       <div className="collapse navbar-collapse text-right" id="navbarNav">
         <ul className="navbar-nav ml-auto pl-0">
           <li className="nav-item active">
-            <a className="nav-link navBtn" href="/">Home</a>
+            <p className="nav-link navBtn" onClick={() => history.push('/')}>Home</p>
           </li>
-          
+
           <Dropdown className="dropdown">
             <Dropdown.Toggle as={CustomToggle} id="dropdownToggle">
               Account
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-              <Dropdown.Item href="/account">Account Information</Dropdown.Item>
+              <Dropdown.Item onClick={() => history.push('/account')}>Account Information</Dropdown.Item>
               <Dropdown.Divider />
-              <Dropdown.Item> 
-              {(user && user.isLoggedIn) ? (
-                <Dropdown.Item href="/" onClick={logout}>Logout</Dropdown.Item>
-              ) : (
-                  <Dropdown.Item href="/">Login</Dropdown.Item>
-              )}
-              </Dropdown.Item>
+                  <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-          
-          
+
+
         </ul>
       </div>
     </nav>
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,26 +1,31 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import AUTH from "../utils/AUTH";
 import "../assets/scss/register.scss";
 import BrandIcon from '../assets/images/icons/BrandIcon.png'
 
 const Register = () => {
+    const history = useHistory();
     const [userObject, setUserObject] = useState({
         firstName: "",
         lastName: "",
         username: "",
         password: "",
         confirmPassword: "",
-        redirectTo: null
     });
-
-    const [redirectTo, setRedirectTo] = useState(null);
+    // const [disabled, setDisabled] = useState(true)
 
     const handleChange = (e) => {
         setUserObject({
             ...userObject,
             [e.target.name]: e.target.value
         });
+
+        // (userObject.firstName && userObject.lastName &&
+        //     userObject.username && userObject.password &&
+        //     userObject.confirmPassword)
+        //     ? setDisabled(false)
+        //     : setDisabled(true)
     };
 
     const handleSubmit = (e) => {
@@ -34,24 +39,20 @@ const Register = () => {
             email: userObject.email,
             password: userObject.password,
         })
-        .then(response => {
-            console.log(response);
-            if (!response.data.errmsg) {
-                setRedirectTo('/dashboard')
-            } else {
-                console.log('duplicate');
-            }
-        });
-
-        if (redirectTo) {
-            return <Redirect to= {{pathname: redirectTo }} />
-        };
+            .then(response => {
+                console.log(response);
+                if (!response.data.errmsg) {
+                    alert('Successful Creation')
+                } else {
+                    console.log('duplicate');
+                }
+            });
     };
 
     return (
         <div>
             <nav className="navbar navTitleBar navbar-expand-lg fixed-top">
-                <img id="brandIcon" className=" mx-auto image-fluid" alt="Pickup" src={BrandIcon}/>
+                <img id="brandIcon" className=" mx-auto image-fluid" alt="Pickup" src={BrandIcon} />
                 <div className="navbar-brand appTitle ml-4">Donation Connection</div>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -67,111 +68,111 @@ const Register = () => {
 
             <div className="container registerPage">
                 <div className="headingRegister">
-                    <h2 style={{color: "black"}}>Registration Form</h2>
-                </div>                
+                    <h2 style={{ color: "black" }}>Registration Form</h2>
+                </div>
                 <form className="row g-1 needs-validation" noValidate>
-                    
+
                     <div className="col-md-10 registerField">
                         <label for="validationCustom01" className="form-label">First name</label>
                         <input type="text"
-                        name="firstName"
-                        className="form-control" 
-                        id="validationCustom01"
-                        placeholder="Ex. Jane"
-                        onChange={handleChange} 
-                        value={userObject.firstName} 
-                        required/>
+                            name="firstName"
+                            className="form-control"
+                            id="validationCustom01"
+                            placeholder="Ex. Jane"
+                            onChange={handleChange}
+                            value={userObject.firstName}
+                            required />
                         <div className="valid-feedback">
-                        Looks good!
+                            Looks good!
                         </div>
                     </div>
                     <div className="col-md-10 registerField">
                         <label for="validationCustom02" className="form-label">Last name</label>
-                        <input type="text" 
-                        name="lastName"
-                        className="form-control" 
-                        id="validationCustom02"
-                        placeholder="Ex. Joe"
-                        onChange={handleChange} 
-                        value={userObject.lastName} 
-                        required />
+                        <input type="text"
+                            name="lastName"
+                            className="form-control"
+                            id="validationCustom02"
+                            placeholder="Ex. Joe"
+                            onChange={handleChange}
+                            value={userObject.lastName}
+                            required />
                         <div className="valid-feedback">
-                        Looks good!
+                            Looks good!
                         </div>
                     </div>
                     <div className="col-md-10 registerField">
                         <label for="validationCustomUsername" className="form-label">Username</label>
                         <div className="input-group has-validation">
-                        <input type="text" 
-                        name="username"
-                        className="form-control" 
-                        id="validationCustomUsername" 
-                        placeholder="Ex. janejoe"
-                        aria-describedby="inputGroupPrepend" 
-                        onChange={handleChange} 
-                        value={userObject.username} required />
-                        <div className="invalid-feedback">
-                            Please choose a username.
+                            <input type="text"
+                                name="username"
+                                className="form-control"
+                                id="validationCustomUsername"
+                                placeholder="Ex. janejoe"
+                                aria-describedby="inputGroupPrepend"
+                                onChange={handleChange}
+                                value={userObject.username} required />
+                            <div className="invalid-feedback">
+                                Please choose a username.
                         </div>
                         </div>
                     </div>
                     <div className="col-md-10 registerField">
                         <label for="validationCustomEmail" className="form-label">E-mail</label>
                         <div className="input-group has-validation">
-                        <input type="text" 
-                        name="email"
-                        className="form-control" 
-                        id="validationCustomEmail"
-                        placeholder="Ex. jjoe@email.com" 
-                        aria-describedby="inputGroupPrepend" 
-                        onChange={handleChange} 
-                        value={userObject.email} required />
-                        <div className="invalid-feedback">
-                            Please choose an e-mail.
+                            <input type="text"
+                                name="email"
+                                className="form-control"
+                                id="validationCustomEmail"
+                                placeholder="Ex. jjoe@email.com"
+                                aria-describedby="inputGroupPrepend"
+                                onChange={handleChange}
+                                value={userObject.email} required />
+                            <div className="invalid-feedback">
+                                Please choose an e-mail.
                         </div>
                         </div>
                     </div>
                     <div className="col-md-10 registerField">
                         <label for="validationCustomPassword" className="form-label">Password</label>
                         <div className="input-group has-validation">
-                        <input type="password" 
-                        name="password"
-                        className="form-control" 
-                        id="validationCustomPassword" 
-                        placeholder="Ex. 123"
-                        aria-describedby="inputGroupPrepend" 
-                        onChange={handleChange} 
-                        value={userObject.password} 
-                        required />
-                        <div className="invalid-feedback">
-                            Please enter a password.
+                            <input type="password"
+                                name="password"
+                                className="form-control"
+                                id="validationCustomPassword"
+                                placeholder="Ex. 123"
+                                aria-describedby="inputGroupPrepend"
+                                onChange={handleChange}
+                                value={userObject.password}
+                                required />
+                            <div className="invalid-feedback">
+                                Please enter a password.
                         </div>
                         </div>
                     </div>
                     <div className="col-md-10 registerField">
                         <label for="validationCustomPassword" className="form-label">Confirm password</label>
                         <div className="input-group has-validation">
-                        <input type="password" 
-                        name="confirmPassword"
-                        className="form-control" 
-                        id="validationCustomConfirmPassword" 
-                        placeholder="Ex. 123"
-                        aria-describedby="inputGroupPrepend" 
-                        onChange={handleChange} 
-                        value={userObject.confirmPassword} 
-                        required />
-                        <div className="invalid-feedback">
-                            Please confirm your password.
-                        </div>
+                            <input type="password"
+                                name="confirmPassword"
+                                className="form-control"
+                                id="validationCustomConfirmPassword"
+                                placeholder="Ex. 123"
+                                aria-describedby="inputGroupPrepend"
+                                onChange={handleChange}
+                                value={userObject.confirmPassword}
+                                required />
+                            <div className="invalid-feedback">
+                                Please confirm your password.
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-10 floater ">
+                        <button className="btn btn-info logBtn ml-3 mr-0 press-on" onClick={() => history.push('/login')} >Login</button>
                         <button className="btn btn-info registrationBtn ml-3 mr-0 press-on" onClick={handleSubmit}>Register Now</button>
-                        <a href="/login" className="logBtn">Login</a>
                     </div>
-                   
+
                 </form>
-                </div>
+            </div>
         </div>
     );
 }
