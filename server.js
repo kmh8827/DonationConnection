0b0da10b3e6bbb5c3d3c91c278c8235acc80da60
kevin <kevinhellman94@gmail.com>
2021-05-18 13:20:37 -0400

    Fixed Typo

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(passport.session());
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
 	// console.log('YOU ARE IN THE PRODUCTION ENV');
-	app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
+	app.use('/static', express.static(path.join(__dirname, './client/build/static')));
 	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, '../client/build/'))
 	});
